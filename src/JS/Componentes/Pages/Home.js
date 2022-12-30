48abd3c31027b500979daacf39c0e1296e756083
*Diogo <diogolimakas@hotmail.com>
2022-12-30 20:35:03 -0300

    adição do componente parceiros e modificação style css para implementação da responsividade das logos

--- src/JS/Componentes/Pages/Home.js
+++ src/JS/Componentes/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Introducao from '../Todos/Introducao.js'
+import Parceiros from '../Todos/Parceiros.js'
 const Home = () => {
   return (
     <div>
       <Introducao/>
+      <Parceiros/>
     </div>
   )
 }
--- src/JS/Componentes/Todos/Parceiros.js
+++ src/JS/Componentes/Todos/Parceiros.js
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import Motors from '../../../assests/parceiros/motors.svg'
+import Decora from '../../../assests/parceiros/decora.svg'
+import Sixcode from '../../../assests/parceiros/six.svg'
 const Parceiros = () => {
   return (
-    <div>Parceiros</div>
+    <section className='cor-b-2 py-md-6 py-4'>
+
+      <div className='container-md container-fluid'>
+      <h1 className='fw-semibold mb-4 cor-p1 text-center'>PARCEIROS</h1>
+      <div className='row d-sm-flex d-grid gap-3 align-items-center mx-2 logo-parceiros text-center  align-items-center'>
+        <div className='col d-flex rounded p-3 cor-b-1 shadow justify-content-center'>
+          <img src={Decora} alt="" />
+        </div>
+        <div className='col d-flex justify-content-center rounded p-3 cor-b-1 shadow'>
+          <img src={Motors} alt="" />
+        </div>
+        <div className='col d-flex justify-content-center rounded p-3 cor-b-1 shadow'>
+          <img src={Sixcode} alt="" />
+        </div>
+      </div>
+      </div>
+     
+    </section>
   )
 }
 
